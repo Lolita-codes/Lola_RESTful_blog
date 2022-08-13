0901de849df6dc5d3216aa8a95df53c506febc36
Lolita <97053333+Lolita-codes@users.noreply.github.com>
2022-08-13 17:59:16 +0100

    Add Register new users and login registered users

--- forms.py
+++ forms.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL
+from wtforms.validators import DataRequired, URL, Email
 from flask_ckeditor import CKEditorField
+
 
 # WTForm
 class CreatePostForm(FlaskForm):
@@ -10,4 +11,17 @@ class CreatePostForm(FlaskForm):
     img_url = StringField("Blog Image URL", validators=[DataRequired(), URL()])
     body = CKEditorField("Blog Content", validators=[DataRequired()])
     submit = SubmitField("Submit Post")
+
+
+class RegisterForm(FlaskForm):
+    email = StringField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    submit = SubmitField("Sign me up")
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Let me in')
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import datetime
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request, flash
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import relationship
 from flask_ckeditor import CKEditor
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
-from forms import CreatePostForm
+from forms import CreatePostForm, RegisterForm, LoginForm
 from flask_gravatar import Gravatar
 import os
 from dotenv import load_dotenv
@@ -24,6 +24,10 @@ db = SQLAlchemy(app)
 current_year = datetime.datetime.now().year
 
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
 # CONFIGURE TABLE
 class BlogPost(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -34,6 +38,20 @@ class BlogPost(UserMixin, db.Model):
     author = db.Column(db.String(250), nullable=False)
     img_url = db.Column(db.String(250), nullable=False)
 # db.create_all()
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(100))
+#db.create_all()
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 
 # Gets Blog posts items
 @app.route('/')
@@ -107,14 +125,57 @@ def delete_post(post_id):
     return redirect(url_for('get_all_posts'))
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    pass
+    form = LoginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        # Find user by email entered.
+        user = User.query.filter_by(email=email).first()
+        # Email doesn't exist
+        if not user:
+            flash('That email does not exist, please try again.')
+        # Check stored password hash against entered password hashed.
+        elif not check_password_hash(user.password, password):
+            flash('Password incorrect, please try again.')
+        # Email exists and password correct
+        else:
+            login_user(user)
+            return redirect(url_for('get_all_posts'))
+
+    return render_template("login.html", form=form, logged_in=current_user.is_authenticated)
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    pass
+    form = RegisterForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        user = User.query.filter_by(email=email).first()
+        # User already exists
+        if user:
+            flash('You\'ve already signed up with that email, log in instead!')
+            return redirect(url_for('login'))
+        hashed_and_salted_password = generate_password_hash(form.password.data,
+                                                            method='pbkdf2:sha256',
+                                                            salt_length=8)
+        new_user = User()
+        new_user.email = form.email.data
+        new_user.name = form.name.data
+        new_user.password = hashed_and_salted_password
+        db.session.add(new_user)
+        db.session.commit()
+        # Log in and authenticate user after adding details to database.
+        login_user(new_user)
+        return redirect(url_for('get_all_posts'))
+    return render_template("register.html", form=form, logged_in=current_user.is_authenticated)
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('get_all_posts'))
 
 
 if __name__ == "__main__":
